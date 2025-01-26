b36f707f32ec21ac258b4768b6d0ff613dfeeae7
Mohitrawal7 <rawalmohit12@gmail.com>
2025-01-26 23:05:34 +0545

    docker compose and build

---
+++ getting-started-todo-app-main/backend/src/routes/getGreeting.js
@@ -0,0 +1,11 @@
+const GREETINGS = [
+    "Whale-come!",
+    "All hands on deck!",
+    "Charting the course ahead!",
+];
+
+module.exports = async (req, res) => {
+    res.send({
+        greeting: GREETINGS[ Math.floor( Math.random() * GREETINGS.length )],
+    });
+};
